bc5983109ca8cbfe75bca9e35f82143a244a8cfc
DancingLeaf <851280706@qq.com>
2024-11-16 19:33:16 +0800

    feat: kbase item

--- server/vue3/v3-admin-vite/src/views/table/kbase/components/Kbitem.vue
+++ server/vue3/v3-admin-vite/src/views/table/kbase/components/Kbitem.vue
@@ -1,19 +1,21 @@
 <script lang="ts" setup>
 import { reactive, ref, onMounted } from "vue"
-
 defineOptions({
   // 命名当前组件
   name: "Kbitem"
 })
+
+const url =
+  'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg'
 </script>
 <template>
-  <el-card style="max-width: 480px">
-    <div>
-      <div>图标</div>
-      <div>
-        <div>
+  <el-card style="max-width: 480px" shadow="hover"  >
+    <div style="display: flex;">
+      <div><el-image style="width: 48px; height: 48px" :src="url" fit="fill" /></div>
+      <div style="margin-left: 16px;">
+        <div>title</div>
+        <div>tag</div>
       </div>
-    <div></div></div>
     </div>
     <template #footer>Footer content</template>
   </el-card>
